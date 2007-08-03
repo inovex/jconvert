3eed74e41117a95005ce1f56149e3dacd4b91d8b
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-08-03 00:25:54 +0000

    New logger method and added an equals method
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@16 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/domain/ConversionType.java
+++ src/com/edsdev/jconvert/domain/ConversionType.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
 
+import com.edsdev.jconvert.util.Logger;
+
 /**
  * Simple domain object that facilitates segregation of like conversions. For example, there will be a conversion type
  * for time that will contain many conversions, like minutes to seconds and hours to weeks.
@@ -11,6 +13,8 @@ import java.util.Iterator;
  * @author Ed Sarrazin Created on Jul 14, 2007 10:06:32 AM
  */
 public class ConversionType {
+    private static Logger log = Logger.getInstance(ConversionType.class);
+    
     private String typeName;
 
     private Collection conversions = new HashSet();
@@ -35,9 +39,17 @@ public class ConversionType {
         Iterator iter = conversions.iterator();
         while (iter.hasNext()) {
             Conversion conv = (Conversion) iter.next();
-            System.out.println(typeName + ": " + conv.getFromUnit() + " TO " + conv.getToUnit() + " : "
+            log.debug(typeName + ": " + conv.getFromUnit() + " TO " + conv.getToUnit() + " : "
                     + conv.getFromToFactor() + " - " + conv.getGenerationAge());
         }
+    }
+    public boolean equals(Object obj) {
+        if (obj instanceof ConversionType) {
+            if (((ConversionType)obj).getTypeName().equals(this.getTypeName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
