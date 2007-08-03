227cfba8520869a59cc0cf6a6ad7fb210862bee1
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-08-03 00:15:12 +0000

    Updated the logic for loading data to make the loading of data faster.  The assumption is that the standard data in convert.dat needs no data generation, but the conver_custom.dat file will.  The new logging was also implemented.  Also added a new helper method for exporting generated data to help in creating new files without the need for generation
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@13 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/persistence/DataLoader.java
+++ src/com/edsdev/jconvert/persistence/DataLoader.java
@@ -1,11 +1,14 @@
 package com.edsdev.jconvert.persistence;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -14,7 +17,9 @@ import java.util.StringTokenizer;
 
 import com.edsdev.jconvert.domain.Conversion;
 import com.edsdev.jconvert.domain.ConversionType;
+import com.edsdev.jconvert.domain.FractionalConversion;
 import com.edsdev.jconvert.logic.ConversionGapBuilder;
+import com.edsdev.jconvert.util.Logger;
 import com.edsdev.jconvert.util.ResourceManager;
 
 /**
@@ -35,13 +40,12 @@ import com.edsdev.jconvert.util.ResourceManager;
  * @author Ed Sarrazin Created on Jul 17, 2007 6:59:31 PM
  */
 public class DataLoader {
-
-    private boolean generateGaps = true;
+    private static Logger log = Logger.getInstance(DataLoader.class);
 
     public static void main(String[] args) {
         StringTokenizer stoken = new StringTokenizer("test1,test2,,test4".replaceAll(",", ", "), ",");
         while (stoken.hasMoreTokens()) {
-            System.out.println(stoken.nextToken());
+            log.debug(stoken.nextToken());
         }
 
     }
@@ -81,11 +85,11 @@ public class DataLoader {
                 }
             }
         } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+            log.error("Could not load data from stream", ex);
         } catch (IOException ex) {
-            ex.printStackTrace();
+            log.error("Could not load data from stream", ex);
         } catch (Exception ex) {
-            ex.printStackTrace();
+            log.error("Could not load data from stream", ex);
         } finally {
             try {
                 if (input != null) {
@@ -93,7 +97,7 @@ public class DataLoader {
                     input.close();
                 }
             } catch (IOException ex) {
-                ex.printStackTrace();
+                log.error("Could not load data from stream", ex);
             }
         }
 
@@ -106,32 +110,125 @@ public class DataLoader {
      * @return List of ConversionTypes
      */
     public List loadData() {
-        // return getHardCodedData();
+        //load each of the two files
+        List main = loadData("convert.dat", false, null);
+        List addOns = loadData("convert_custom.dat", true, main);
+
+        //merge the data between the two.
+        List rv = new ArrayList();
+        rv.addAll(main);
+        Iterator iter = addOns.iterator();
+        while (iter.hasNext()) {
+            ConversionType type = (ConversionType) iter.next();
+            if (rv.contains(type)) {
+                for (int i = 0; i < rv.size(); i++) {
+                    ConversionType existing = (ConversionType) rv.get(i);
+                    if (existing.equals(type)) {
+                        mergeConversionType(existing, type);
+                        break; //get out of for loop, we are done.
+                    }
+                }
+            } else {
+                rv.add(type);
+            }
+        }
+
+        return rv;
+    }
+
+    /**
+     * This method is responsible for merging the ConversionType data into the existing ConversionType data. It uses
+     * some logic to determine if the new data is non-existent in the existing data. If so it adds it. If it already
+     * exists in the existing data, then it compares the two Conversions and determines which conversion to use. This is
+     * simple logic looking at the generation age of the data.
+     * 
+     * @param dest ConversionType data that we want to merge into. This is considered existing data
+     * @param newData ConversionType data that we want to merge into the destination.
+     */
+    private void mergeConversionType(ConversionType dest, ConversionType newData) {
+        Iterator iter = newData.getConversions().iterator();
+        while (iter.hasNext()) {
+            Conversion newConversion = (Conversion) iter.next();
+            if (dest.getConversions().contains(newConversion)) {
+                Iterator iter2 = dest.getConversions().iterator();
+                while (iter2.hasNext()) {
+                    Conversion destConversion = (Conversion) iter2.next();
+                    if (destConversion.equals(newConversion)) {
+                        if (destConversion.getGenerationAge() >= newConversion.getGenerationAge()) {
+                            dest.getConversions().remove(destConversion);
+                            dest.getConversions().add(newConversion);
+                        }
+                        break; //shortcut out of while
+                    }
+                }
+            } else {
+                dest.getConversions().add(newConversion);
+            }
+        }
+    }
+
+    /**
+     * This method is responsible for loading data from a resource into a list. existing conversion types are passed
+     * into this method to provide support for generating gaps. For example if you create a conversion of decades to
+     * doubledecades and that is it. We will look at the existing conversions to also generate gaps such as
+     * doubledecades to year, months, days, etc.
+     * 
+     * @param resourceName String name of the resource that has the data to be loaded
+     * @param generateGaps boolean that indicates whether or not to spend time generating missing conversions. This is
+     *        needed because generating these gaps takes significant time and the default conversions that are supplied
+     *        will have all combinatorics created before hand
+     * @param existingConversionTypes List of existing conversion types that will be used in aiding generation of
+     *        missing conversion types.
+     * @return List of ConversionTypes.
+     */
+    private List loadData(String resourceName, boolean generateGaps, List existingConversionTypes) {
         HashMap map = new HashMap();
         Date date1 = new Date();
 
-        loadDataFromStream(ResourceManager.getResourceAsStream("convert.dat"), map);
-        loadDataFromStream(ResourceManager.getResourceAsStream("convert_custom.dat"), map);
+        loadDataFromStream(ResourceManager.getResourceAsStream(resourceName), map);
 
         Date date2 = new Date();
-        System.out.println("Loaded file - " + (date2.getTime() - date1.getTime()) + " miliseconds");
+        log.debug("Loaded file - " + (date2.getTime() - date1.getTime()) + " miliseconds");
         List rv = new ArrayList(map.values());
+
+        Iterator iter = rv.iterator();
+        while (iter.hasNext()) {
+            ConversionType ct = (ConversionType) iter.next();
+            ConversionGapBuilder.createOneToOneConversions(ct);
+        }
 
         if (!generateGaps) {
             return rv;
         }
 
-        Iterator iter = rv.iterator();
+        iter = rv.iterator();
         while (iter.hasNext()) {
             ConversionType ct = (ConversionType) iter.next();
+            Iterator existingIter = existingConversionTypes.iterator();
+            while (existingIter.hasNext()) {
+                ConversionType existingCt = (ConversionType) existingIter.next();
+                if (ct.getTypeName().equals(existingCt.getTypeName())) {
+                    ct.getConversions().addAll(existingCt.getConversions());
+                    break;
+                }
+            }
             ConversionGapBuilder.createMissingConversions(ct);
         }
 
         date1 = new Date();
-        System.out.println("Created conversions - " + (date1.getTime() - date2.getTime()) + " miliseconds");
+        log.debug("Created conversions - " + (date1.getTime() - date2.getTime()) + " miliseconds");
+        //        this.unloadData(rv);
         return rv;
     }
 
+    /**
+     * Simply looks in the map for the conversionType (indicated by the string) and returns it. If it is not found, then
+     * it creates it, puts it in the HashMap and returns it to you.
+     * 
+     * @param map HashMap of existing ConversionTypes
+     * @param type String name of the conversion type
+     * @return ConversionType
+     */
     private ConversionType getConversionType(HashMap map, String type) {
         if (map.containsKey(type)) {
             return (ConversionType) map.get(type);
@@ -142,52 +239,39 @@ public class DataLoader {
         return ct;
     }
 
-    private List getHardCodedData() {
-        List rv = new ArrayList();
-
-        Conversion c1 = Conversion.createInstance("minutes", "'", "hours", "hrs",
-            "0.016666666666666666666666666666667", 0);
-        Conversion c2 = Conversion.createInstance("hours", "hrs", "seconds", "''", "3600", 0);
-        Conversion c3 = Conversion.createInstance("minutes", "'", "day", "", "0.00069444444444444444444444444444444", 0);
-        Conversion c4 = Conversion.createInstance("year", "", "month", "", "12", 0);
-        Conversion c5 = Conversion.createInstance("year", "", "month (30 days)", "", "12.16666667", 0);
-        Conversion c6 = Conversion.createInstance("month (30 days)", "", "day", "", "30", 0);
-
-        ConversionType ct = new ConversionType();
-        ct.setTypeName("Time");
-
-        ct.addConversion(c1);
-        ct.addConversion(c2);
-        ct.addConversion(c3);
-        ct.addConversion(c4);
-        ct.addConversion(c5);
-        ct.addConversion(c6);
-
-        ConversionGapBuilder.createMissingConversions(ct);
-
-        rv.add(ct);
-
-        c1 = Conversion.createInstance("C", "", "miles/hour", "mph", "670616629.4", 0);
-        c2 = Conversion.createInstance("C", "", "feet/second", "fps", "983571056.4", 0);
-        c3 = Conversion.createInstance("C", "", "meters/second", "", "299792458", 0);
-        c4 = Conversion.createInstance("C", "", "knot", "", "582749918.4", 0);
-        c5 = Conversion.createInstance("C", "", "mach", "", "904460.4418", 0);
-        c6 = Conversion.createInstance("C", "", "kilometer/hour", "kph", "1079252849", 0);
-
-        ct = new ConversionType();
-        ct.setTypeName("Speed");
-
-        ct.addConversion(c1);
-        ct.addConversion(c2);
-        ct.addConversion(c3);
-        ct.addConversion(c4);
-        ct.addConversion(c5);
-        ct.addConversion(c6);
-
-        ConversionGapBuilder.createMissingConversions(ct);
-
-        rv.add(ct);
-
-        return rv;
+    /**
+     * Writes the List of ConversionTypes to output.dat.
+     * 
+     * @param domainData List of ConversionTypes
+     */
+    public void unloadData(List domainData) {
+        //iterate through conversiontypes and write out.
+        Iterator iter = domainData.iterator();
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.dat"));
+            while (iter.hasNext()) {
+                ConversionType ct = (ConversionType) iter.next();
+                List lst = new ArrayList();
+                lst.addAll(ct.getConversions());
+                Collections.sort(lst);
+                Iterator iter2 = lst.iterator();
+                while (iter2.hasNext()) {
+                    Conversion c = (Conversion) iter2.next();
+                    if (c instanceof FractionalConversion) {
+                        writer.write(ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
+                                + c.getToUnit() + "," + c.getToUnitAbbr() + "," + c.getFromToTopFactor() + "/"
+                                + c.getFromToBottomFactor() + "," + c.getFromToOffset() + "\n");
+                    } else {
+                        writer.write(ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
+                                + c.getToUnit() + "," + c.getToUnitAbbr() + "," + c.getFromToFactor() + ","
+                                + c.getFromToOffset() + "\n");
+                    }
+                }
+            }
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
+
 }
