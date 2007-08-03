549589d56557676c340854f89f4157894a00ce6b
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-08-03 00:25:17 +0000

    implemented new logging. Also has a reduceFraction method added that is not used at the moment
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@15 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/domain/FractionalConversion.java
+++ src/com/edsdev/jconvert/domain/FractionalConversion.java
@@ -1,10 +1,14 @@
 package com.edsdev.jconvert.domain;
 
+import com.edsdev.jconvert.util.Logger;
+
 public class FractionalConversion extends Conversion {
 
     private long fromToTopFactor = 1;
 
     private long fromToBottomFactor = 1;
+    
+    private static Logger log = Logger.getInstance(FractionalConversion.class);
 
     public FractionalConversion(String fromUnit, String fromUnitAbbr, String toUnit, String toUnitAbbr,
             String fromToFactor, double fromToOffset) {
@@ -93,6 +97,8 @@ public class FractionalConversion extends Conversion {
     public void setFromToFactorString(String fromToFactor) {
         int pos = fromToFactor.indexOf("/");
         if (pos > 0) {
+//            fromToFactor = reduceFraction(fromToFactor);
+//            pos = fromToFactor.indexOf("/");
             String top = fromToFactor.substring(0, pos);
             String bottom = fromToFactor.substring(pos + 1);
             fromToTopFactor = getLong(top);
@@ -102,10 +108,67 @@ public class FractionalConversion extends Conversion {
             fromToTopFactor = getLong(fromToFactor);
             fromToBottomFactor = 1;
         } else {
-            System.err.println("Tried to process decimal as fraction:" + fromToFactor);
+            log.error("Tried to process decimal as fraction:" + fromToFactor);
             //TODO throw proper exception here
         }
     }
+	private static long getTop(String val) {
+		int pos = val.indexOf("/");
+		return Long.parseLong(val.substring(0, pos));
+	}
+
+	private static long getBottom(String val) {
+		int pos = val.indexOf("/");
+		return Long.parseLong(val.substring(pos + 1, val.length()));
+	}
+
+	private static String reduceFraction(String val) {
+		long top = getTop(val);
+		long bottom = getBottom(val);
+
+		long largest = top;
+		if (largest < bottom) {
+			largest = bottom;
+		}
+		boolean reduction = true;
+
+		while (reduction) {
+			reduction = false;
+
+			for (double i = 2; i <= 100000; i++) {
+				double result = top / i;
+				if (result < i) {
+					break;
+				}
+				if (isInteger(result)) {
+					double result2 = bottom / i; 
+					if (isInteger(result2)) {
+						reduction = true;
+						top = Math.round(result);
+						bottom = Math.round(result2);
+						break;
+					}
+				}
+			}
+		}
+		return top + "/" + bottom;
+
+	}
+	private static boolean isInteger(double val) {
+		String str = val + "";
+		boolean rv = true;
+		int pos = str.indexOf(".");
+		if (pos < 0) {
+			return rv;
+		}
+		for (int i = pos + 1; i < str.length();i++) {
+			if (!str.substring(i, i + 1).equals("0")) {
+				return false;
+			}
+		}
+		return rv;
+	}
+
 
     /* (non-Javadoc)
      * @see com.edsdev.jconvert.domain.Conversion#getFromToFactorString()
