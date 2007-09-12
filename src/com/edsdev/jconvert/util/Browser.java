458c89b560e5f97e4361a6a326dbb302741aadf9
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-09-12 01:10:59 +0000

    New class that handles launching of a browser, etc.
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@38 af8f7118-8b35-0410-841c-bf10d0a9d012

---
+++ src/com/edsdev/jconvert/util/Browser.java
@@ -0,0 +1,49 @@
+package com.edsdev.jconvert.util;
+
+import java.lang.reflect.Method;
+
+import javax.swing.JOptionPane;
+
+/**
+ * This is a very lightweight browser support class. It only supports windows, mac, unix, and linux.
+ * 
+ * @author Ed S Created on Sep 10, 2007 9:02:07 PM
+ */
+public class Browser {
+    private static final String errMsg = "Error attempting to launch web browser";
+
+    /**
+     * Opens the browser with the supplied URL
+     * 
+     * @param url String representation of the URL
+     */
+    public static void openURL(String url) {
+        String osName = System.getProperty("os.name");
+        try {
+            if (osName.startsWith("Mac OS")) {
+                Class fileMgr = Class.forName("com.apple.eio.FileManager");
+                Method openURL = fileMgr.getDeclaredMethod("openURL", new Class[] { String.class });
+                openURL.invoke(null, new Object[] { url });
+            } else if (osName.startsWith("Windows")) {
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else {
+                //assume Unix or Linux
+                String[] browsers = { "firefox", "opera", "konqueror", "epiphany", "mozilla", "netscape" };
+                String browser = null;
+                for (int count = 0; count < browsers.length && browser == null; count++) {
+                    if (Runtime.getRuntime().exec(new String[] { "which", browsers[count] }).waitFor() == 0) {
+                        browser = browsers[count];
+                    }
+                }
+                if (browser == null) {
+                    throw new Exception("Could not find web browser");
+                } else {
+                    Runtime.getRuntime().exec(new String[] { browser, url });
+                }
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, errMsg + ":\n" + e.getLocalizedMessage());
+        }
+    }
+
+}
