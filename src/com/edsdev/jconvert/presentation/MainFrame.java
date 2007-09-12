7ac82ee149dfc0dc7622818f1de1156fc75d95c1
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-09-12 01:21:19 +0000

    added a menu item to the main frame and also added the calls to the about dialog
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@43 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/presentation/MainFrame.java
+++ src/com/edsdev/jconvert/presentation/MainFrame.java
@@ -1,11 +1,16 @@
 package com.edsdev.jconvert.presentation;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.UIManager;
@@ -17,6 +22,8 @@ import com.edsdev.jconvert.util.ResourceManager;
 public class MainFrame extends JFrame {
 
     private static final long serialVersionUID = 1L;
+    
+    private AboutDialog aboutDlg = null;
 
     private List data;
 
@@ -42,6 +49,7 @@ public class MainFrame extends JFrame {
         this.setSize(600, 400);
 
         this.setTitle("JConvert");
+        this.setJMenuBar(getMenu());
 
         JTabbedPane tabbedPane = new JTabbedPane();
         // tabbedPane.addChangeListener(new ChangeListener() {
@@ -61,9 +69,47 @@ public class MainFrame extends JFrame {
     }
 
     private JPanel getNewPanel(ConversionTypeData ctd) {
-
         return new ConversionPanel(ctd);
+    }
 
+    private JMenuBar getMenu() {
+        JMenuBar bar = new JMenuBar();
+
+        JMenu fileMenu = new JMenu("File");
+        fileMenu.setMnemonic('F');
+
+        JMenuItem fileMenuExit = new JMenuItem("Exit");
+        fileMenuExit.setMnemonic('X');
+        fileMenuExit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        fileMenu.add(fileMenuExit);
+
+        JMenu helpMenu = new JMenu("Help");
+        helpMenu.setMnemonic('H');
+
+        JMenuItem helpMenuAbout = new JMenuItem("About");
+        helpMenuAbout.setMnemonic('A');
+        helpMenuAbout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                displayAboutDialog();
+            }
+        });
+        helpMenu.add(helpMenuAbout);
+
+        bar.add(fileMenu);
+        bar.add(helpMenu);
+
+        return bar;
+    }
+    private void displayAboutDialog() {
+        if (aboutDlg == null) {
+            aboutDlg = new AboutDialog(MainFrame.this);
+        }
+        aboutDlg.setVisible(true);
     }
 
     private List getData() {
