17560d4d8d9b74695e7f1b46ce288f97b0e003d9
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-09-21 23:23:56 +0000

    new methods for obtaining the path to the jar file so that we can create files in a reasonable directory.  This also helps the problem of launching the jar directly from the download page and the file getting created outside the classpath
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@56 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/util/ResourceManager.java
+++ src/com/edsdev/jconvert/util/ResourceManager.java
@@ -8,6 +8,8 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.Properties;
 
 import javax.swing.ImageIcon;
@@ -19,6 +21,12 @@ import javax.swing.ImageIcon;
 public class ResourceManager {
     private static Logger log = Logger.getInstance(ResourceManager.class);
 
+    /**
+     * Loads and Image from the classpath as a resource
+     * 
+     * @param imageRelativeURL
+     * @return Image object
+     */
     public static Image getImage(String imageRelativeURL) {
         Image image = null;
         try {
@@ -30,10 +38,23 @@ public class ResourceManager {
         return image;
     }
 
+    /**
+     * Loads a properties file from the classpath based on the name provided
+     * 
+     * @param filename
+     * @return Properties object
+     * @throws java.io.IOException
+     */
     public static Properties loadProperties(String filename) throws java.io.IOException {
         return loadProperties(filename, ResourceManager.class);
     }
 
+    /**
+     * Attempts to find a resource in the classpath and returns an InputStream to that resrouce
+     * 
+     * @param resourceName
+     * @return
+     */
     public static InputStream getResourceAsStream(String resourceName) {
         InputStream is = null;
 
@@ -51,11 +72,53 @@ public class ResourceManager {
         return is;
     }
 
+    /**
+     * Responsible for determining what the path is to the jar file.
+     * 
+     * @return String representation of the path
+     */
+    public static String getJarPath() {
+        String rv = "";
+
+        ClassLoader cl = ResourceManager.class.getClassLoader();
+
+        try {
+            String fileName = "jconvert-" + JConvertProperties.getMajorVersion() + "."
+                    + JConvertProperties.getMinorVersion() + "." + JConvertProperties.getRevision() + ".jar";
+            log.debug("Searching for file " + fileName + " to determine the jar path.");
+            URL url = cl.getResource(fileName);
+            if (url != null) {
+                rv = url.getPath();
+                rv = URLDecoder.decode(rv, System.getProperty("file.encoding"));
+                rv = rv.substring(0, rv.indexOf(fileName));
+            }
+        } catch (Exception e) {
+            log.error("Failed to get the path of the jar", e);
+        }
+
+        return rv;
+    }
+
+    /**
+     * Loads a properties file
+     * 
+     * @param filename Name of the properties file
+     * @param loadClass class used when getting the classloader
+     * @return Properties object loaded with properties from the resource
+     * @throws java.io.IOException
+     */
     public static Properties loadProperties(String filename, Class loadClass) throws java.io.IOException {
         InputStream is = loadClass.getClassLoader().getResourceAsStream(filename);
         return loadProperties(is);
     }
 
+    /**
+     * Loads properties from an InputStream into a Properties object
+     * 
+     * @param Istream
+     * @return Properties object
+     * @throws java.io.IOException
+     */
     public static Properties loadProperties(InputStream Istream) throws java.io.IOException {
         if (Istream == null) {
             System.err.println("Can't load properties file from the inputstream  --  NO resource with the specified name was found");
@@ -78,10 +141,15 @@ public class ResourceManager {
         return props;
     }
 
+    /**
+     * Gets and ImageIcom from the classloader based on the image resource URL
+     * 
+     * @param imageRelativeURL
+     * @return
+     */
     public static ImageIcon getImageIcon(String imageRelativeURL) {
         ImageIcon icon = null;
         try {
-
             ClassLoader cl = ResourceManager.class.getClassLoader();
             icon = new ImageIcon(cl.getResource(imageRelativeURL));
         } catch (Exception exp) {
@@ -89,5 +157,4 @@ public class ResourceManager {
         }
         return icon;
     }
-
 }
