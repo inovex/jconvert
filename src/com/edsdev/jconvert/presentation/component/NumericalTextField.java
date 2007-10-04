7af9e1c7244f42370dfbb5a87fada6e7bb8de401
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-10-04 23:28:28 +0000

    Reworked this component to better handle processing of numbers. It can now handle negatives - in a way that I like.
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@71 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/presentation/component/NumericalTextField.java
+++ src/com/edsdev/jconvert/presentation/component/NumericalTextField.java
@@ -9,46 +9,72 @@ import javax.swing.text.PlainDocument;
 
 public class NumericalTextField extends BaseTextField {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public NumericalTextField(int cols) {
-		super(cols);
-	}
+    public NumericalTextField(int cols) {
+        super(cols);
+    }
 
-	public NumericalTextField() {
-		super();
-	}
+    public NumericalTextField() {
+        super();
+    }
 
-	protected Document createDefaultModel() {
-		return new NumericDocument();
-	}
-
+    protected Document createDefaultModel() {
+        return new NumericDocument(this);
+    }
 }
 
 class NumericDocument extends PlainDocument {
 
-	public NumericDocument() {
-		super();
-	}
+    NumericalTextField parent;
 
-	public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
-		for (int i = 0; i < str.length(); i++) {
-			if (!Character.isDigit(str.charAt(i)) && !new Character(str.charAt(i)).toString().equals(".")) {
-				Toolkit.getDefaultToolkit().beep();
-				return;
-			}
-		}
-		super.insertString(offs, str, a);
-	}
+    public NumericDocument(NumericalTextField theParent) {
+        super();
+        parent = theParent;
+    }
 
-	public void replace(int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
-		for (int i = 0; i < text.length(); i++) {
-			if (!Character.isDigit(text.charAt(i))&& !new Character(text.charAt(i)).toString().equals(".")) {
-				Toolkit.getDefaultToolkit().beep();
-				return;
-			}
-		}
-		super.replace(offset, length, text, attrs);
-	}
+    public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+        super.insertString(offs, str, a);
+    }
+
+    public void replace(int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+        String resultString = "";
+        int negativeCount = 0;
+        // iterate through each character in the string and validate it.
+        for (int i = 0; i < text.length(); i++) {
+            String ch = new Character(text.charAt(i)).toString();
+            if (!Character.isDigit(text.charAt(i)) && !ch.equals(".") && !ch.equals("-") && !ch.equals(",")) {
+                Toolkit.getDefaultToolkit().beep();
+                return;
+            }
+            // if the character is a minus sign, it is ok, but lets remove it and count how many
+            if (ch.equals("-")) {
+                negativeCount++;
+            } else {
+                resultString += ch;
+            }
+        }
+        // stick the value into the field, minus negatives
+        super.replace(offset, length, resultString, attrs);
+
+        // now handle the negatives (if odd number-two negatives make a positive) that you just pulled out.
+        if (negativeCount != 0 && negativeCount % 2 == 1) {
+            String parentText = parent.getText();
+            if (parentText == null || parentText.equals("")) {
+                parentText = "-";
+            } else {
+                // sign needs to change - pull out neg if there is one, add neg if there is not one
+                int pos = parentText.indexOf('-');
+                if (pos >= 0) {
+                    parentText = parentText.replaceAll("-", "");
+                } else {
+                    parentText = "-" + parentText;
+                }
+            }
+            // stick the work that you just did back into the field
+            super.replace(0, parent.getText().length(), parentText, attrs);
+        }
+
+    }
 
 }
