09f8cfafdb7ab083cc7a078e94af8f82ab7284fa
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-10-06 02:04:50 +0000

    Pulling strings from the resource bundle and also making changes for internationalization
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@84 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/presentation/ConversionPanel.java
+++ src/com/edsdev/jconvert/presentation/ConversionPanel.java
@@ -21,6 +21,7 @@ import javax.swing.event.ListSelectionListener;
 import com.edsdev.jconvert.presentation.component.ConvertListCellRenderer;
 import com.edsdev.jconvert.presentation.component.ConvertListModel;
 import com.edsdev.jconvert.presentation.component.NumericalTextField;
+import com.edsdev.jconvert.util.Messages;
 
 public class ConversionPanel extends JPanel {
 
@@ -64,6 +65,27 @@ public class ConversionPanel extends JPanel {
         return value.toString();
     }
 
+    private String getSelectedTranslatedValue(JList theList) {
+        Object value = theList.getSelectedValue();
+        if (value == null) {
+            return null;
+        }
+        if (value instanceof ConversionUnitData) {
+            return ((ConversionUnitData) value).getTranslatedUnit();
+        }
+        return value.toString();
+    }
+
+    private void setSelectedStringValue(JList theList, String value) {
+        for (int i = 0; i < theList.getModel().getSize(); i++) {
+            ConversionUnitData data = (ConversionUnitData) theList.getModel().getElementAt(i);
+            if (data.getUnit().equals(value)) {
+                theList.setSelectedIndex(i);
+                break;
+            }
+        }
+    }
+
     private int getGenerationAge(JList theList) {
         Object value = theList.getSelectedValue();
 
@@ -77,12 +99,36 @@ public class ConversionPanel extends JPanel {
         convert(false);
     }
 
+    public String getFromValue() {
+        return txtFrom.getText();
+    }
+
+    public void setFromValue(String value) {
+        txtFrom.setText(value);
+    }
+
+    public String getFromUnit() {
+        return getSelectedValue(list) + "";
+    }
+
+    public String getToUnit() {
+        return getSelectedValue(list2) + "";
+    }
+
+    public void setFromUnit(String value) {
+        setSelectedStringValue(list, value);
+    }
+
+    public void setToUnit(String value) {
+        setSelectedStringValue(list2, value);
+    }
+
     private void convert(boolean reverse) {
         String fromUnit = getSelectedValue(list);
         String toUnit = getSelectedValue(list2);
         if (fromUnit != null && toUnit != null) {
-            DecimalFormat fmt = new DecimalFormat(
-                "#.######################################################################################################");
+            DecimalFormat fmt = new DecimalFormat();
+            fmt.setMaximumFractionDigits(30);
             if (reverse) {
                 if (txtTo.getText() != null && !txtTo.getText().trim().equals("")) {
                     double startValue = new Double(txtTo.getText()).doubleValue();
@@ -107,7 +153,7 @@ public class ConversionPanel extends JPanel {
         list.setModel(new ConvertListModel(ctd.getAllFromUnits()));
         JScrollPane scrollPanel = new JScrollPane(list);
         scrollPanel.setPreferredSize(scrollPaneSize);
-        scrollPanel.setBorder(BorderFactory.createTitledBorder("From Unit"));
+        scrollPanel.setBorder(BorderFactory.createTitledBorder(Messages.getResource("fromUnitLabel")));
 
         list2 = new JList();
         list2.setModel(new ConvertListModel(new ArrayList()));
@@ -115,7 +161,7 @@ public class ConversionPanel extends JPanel {
 
         JScrollPane scrollPanel2 = new JScrollPane(list2);
         scrollPanel2.setPreferredSize(scrollPaneSize);
-        scrollPanel2.setBorder(BorderFactory.createTitledBorder("To Unit"));
+        scrollPanel2.setBorder(BorderFactory.createTitledBorder(Messages.getResource("toUnitLabel")));
 
         list.addListSelectionListener(new ListSelectionListener() {
             public void valueChanged(ListSelectionEvent e) {
@@ -126,7 +172,8 @@ public class ConversionPanel extends JPanel {
                 String list2Val = getSelectedValue(list2);
 
                 list2.setModel(new ConvertListModel(results));
-                labelFromUnit.setText(selectedValue);
+                list2.setCellRenderer(new ConvertListCellRenderer());
+                labelFromUnit.setText(getSelectedTranslatedValue((JList) e.getSource()));
 
                 if (list2Val != null) {
                     int i = 0;
@@ -145,10 +192,11 @@ public class ConversionPanel extends JPanel {
         list2.addListSelectionListener(new ListSelectionListener() {
             public void valueChanged(ListSelectionEvent e) {
                 // For now assume one tab
-                String selectedValue = getSelectedValue((JList) e.getSource());
+                String selectedValue = getSelectedTranslatedValue((JList) e.getSource());
                 int genAge = getGenerationAge((JList) e.getSource());
                 if (genAge >= 2) {
-                    labelToUnit.setText(selectedValue + " (***-generated conversion(" + genAge + "))");
+                    labelToUnit.setText(selectedValue + " (" + Messages.getResource("generatedConversion") + "("
+                            + genAge + "))");
                 } else {
                     labelToUnit.setText(selectedValue);
                 }
@@ -171,9 +219,9 @@ public class ConversionPanel extends JPanel {
         conversionPanel.setSize(panelSize);
         conversionPanel.setPreferredSize(panelSize);
 
-        JLabel labelFrom = new JLabel("Conversion From");
+        JLabel labelFrom = new JLabel(Messages.getResource("conversionFromLabel"));
         labelFrom.setBounds(5, 5, 100, 22);
-        JLabel labelTo = new JLabel("Conversion To");
+        JLabel labelTo = new JLabel(Messages.getResource("conversionToLabel"));
         labelTo.setBounds(5, 30, 100, 22);
         labelFromUnit = new JLabel("xxx");
         labelFromUnit.setBounds(330, 5, 400, 22);
@@ -192,7 +240,7 @@ public class ConversionPanel extends JPanel {
 
         txtTo = new JTextField();
         txtTo.setBounds(110, 30, 200, 22);
-//        txtTo.setEditable(false);
+        //        txtTo.setEditable(false);
         txtTo.addKeyListener(new KeyAdapter() {
             public void keyReleased(KeyEvent e) {
                 convert(true);
