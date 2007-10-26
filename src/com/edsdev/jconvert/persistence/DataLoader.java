b8349c52413ff8e1f1e792d6ceaf97d3218d4aba
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-10-26 00:11:23 +0000

    Changes to allow parsing of unicode in the conversion file
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@101 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/persistence/DataLoader.java
+++ src/com/edsdev/jconvert/persistence/DataLoader.java
@@ -42,6 +42,8 @@ import com.edsdev.jconvert.util.ResourceManager;
 public class DataLoader {
     private static Logger log = Logger.getInstance(DataLoader.class);
 
+    private static final String specialSaveChars = "=: \t\r\n\f#!";
+
     public static void main(String[] args) {
         StringTokenizer stoken = new StringTokenizer("test1,test2,,test4".replaceAll(",", ", "), ",");
         while (stoken.hasMoreTokens()) {
@@ -64,10 +66,11 @@ public class DataLoader {
         }
         BufferedReader input = null;
         try {
-            input = new BufferedReader(new InputStreamReader(stream));
+            input = new BufferedReader(new InputStreamReader(stream, "8859_1"));
             String line = null;
 
             while ((line = input.readLine()) != null) {
+                line = loadConvert(line);
                 StringTokenizer stok = new StringTokenizer(line.replaceAll(",", ", "), ",");
                 if (!line.startsWith("#") && stok.countTokens() == 7) {
                     String tab = stok.nextToken().trim();
@@ -258,13 +261,15 @@ public class DataLoader {
                 while (iter2.hasNext()) {
                     Conversion c = (Conversion) iter2.next();
                     if (c instanceof FractionalConversion) {
-                        writer.write(ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
+                        String temp = ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
                                 + c.getToUnit() + "," + c.getToUnitAbbr() + "," + c.getFromToTopFactor() + "/"
-                                + c.getFromToBottomFactor() + "," + c.getFromToOffset() + "\n");
+                                + c.getFromToBottomFactor() + "," + c.getFromToOffset() + "\n";
+                        writer.write(saveConvert(temp, false));
                     } else {
-                        writer.write(ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
+                        String temp = ct.getTypeName() + "," + c.getFromUnit() + "," + c.getFromUnitAbbr() + ","
                                 + c.getToUnit() + "," + c.getToUnitAbbr() + "," + c.getFromToFactor() + ","
-                                + c.getFromToOffset() + "\n");
+                                + c.getFromToOffset() + "\n";
+                        writer.write(saveConvert(temp, false));
                     }
                 }
             }
@@ -273,5 +278,144 @@ public class DataLoader {
             e.printStackTrace();
         }
     }
+
+    /**
+     * This method was taken from java.util.Properties Converts encoded &#92;uxxxx to unicode chars and changes special
+     * saved chars to their original forms
+     */
+    private String loadConvert(String theString) {
+        char aChar;
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len);
+
+        for (int x = 0; x < len;) {
+            aChar = theString.charAt(x++);
+            if (aChar == '\\') {
+                aChar = theString.charAt(x++);
+                if (aChar == 'u') {
+                    // Read the xxxx
+                    int value = 0;
+                    for (int i = 0; i < 4; i++) {
+                        aChar = theString.charAt(x++);
+                        switch (aChar) {
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            value = (value << 4) + aChar - '0';
+                            break;
+                        case 'a':
+                        case 'b':
+                        case 'c':
+                        case 'd':
+                        case 'e':
+                        case 'f':
+                            value = (value << 4) + 10 + aChar - 'a';
+                            break;
+                        case 'A':
+                        case 'B':
+                        case 'C':
+                        case 'D':
+                        case 'E':
+                        case 'F':
+                            value = (value << 4) + 10 + aChar - 'A';
+                            break;
+                        default:
+                            throw new IllegalArgumentException("Malformed \\uxxxx encoding.");
+                        }
+                    }
+                    outBuffer.append((char) value);
+                } else {
+                    if (aChar == 't')
+                        aChar = '\t';
+                    else if (aChar == 'r')
+                        aChar = '\r';
+                    else if (aChar == 'n')
+                        aChar = '\n';
+                    else if (aChar == 'f')
+                        aChar = '\f';
+                    outBuffer.append(aChar);
+                }
+            } else
+                outBuffer.append(aChar);
+        }
+        return outBuffer.toString();
+    }
+
+    /**
+     * This method was taken from java.util.Properties Converts unicodes to encoded &#92;uxxxx and writes out any of the
+     * characters in specialSaveChars with a preceding slash
+     */
+    private String saveConvert(String theString, boolean escapeSpace) {
+        int len = theString.length();
+        StringBuffer outBuffer = new StringBuffer(len * 2);
+
+        for (int x = 0; x < len; x++) {
+            char aChar = theString.charAt(x);
+            switch (aChar) {
+            case ' ':
+                if (x == 0 || escapeSpace)
+                    outBuffer.append('\\');
+
+                outBuffer.append(' ');
+                break;
+            case '\\':
+                outBuffer.append('\\');
+                outBuffer.append('\\');
+                break;
+            case '\t':
+                outBuffer.append('\\');
+                outBuffer.append('t');
+                break;
+            case '\n':
+                outBuffer.append('\\');
+                outBuffer.append('n');
+                break;
+            case '\r':
+                outBuffer.append('\\');
+                outBuffer.append('r');
+                break;
+            case '\f':
+                outBuffer.append('\\');
+                outBuffer.append('f');
+                break;
+            default:
+                if ((aChar < 0x0020) || (aChar > 0x007e)) {
+                    outBuffer.append('\\');
+                    outBuffer.append('u');
+                    outBuffer.append(toHex((aChar >> 12) & 0xF));
+                    outBuffer.append(toHex((aChar >> 8) & 0xF));
+                    outBuffer.append(toHex((aChar >> 4) & 0xF));
+                    outBuffer.append(toHex(aChar & 0xF));
+                } else {
+                    if (specialSaveChars.indexOf(aChar) != -1)
+                        outBuffer.append('\\');
+                    outBuffer.append(aChar);
+                }
+            }
+        }
+        return outBuffer.toString();
+    }
+
+    /**
+     * Convert a nibble to a hex character This method was taken from java.util.Properties
+     * 
+     * @param nibble the nibble to convert.
+     */
+    private static char toHex(int nibble) {
+        return hexDigit[(nibble & 0xF)];
+    }
+
+    /**
+     * A table of hex digits This method was taken from java.util.Properties
+     */
+    private static final char[] hexDigit = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+            'F' };
 
 }
