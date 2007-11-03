64916f53ba86f5d9bb1d95d21fa191115f070bb5
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2007-11-03 23:14:04 +0000

    new changes to the layout and presentation
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@124 af8f7118-8b35-0410-841c-bf10d0a9d012

--- src/com/edsdev/jconvert/presentation/UpgradeVersionChecker.java
+++ src/com/edsdev/jconvert/presentation/UpgradeVersionChecker.java
@@ -10,6 +10,7 @@ import java.util.Properties;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
 import com.edsdev.jconvert.util.Browser;
@@ -17,6 +18,7 @@ import com.edsdev.jconvert.util.Http;
 import com.edsdev.jconvert.util.JConvertProperties;
 import com.edsdev.jconvert.util.JConvertSettingsProperties;
 import com.edsdev.jconvert.util.Logger;
+import com.edsdev.jconvert.util.Messages;
 
 /**
  * This is a helper class that is designed to determine if there is a new version of the application that should be
@@ -44,34 +46,34 @@ public class UpgradeVersionChecker {
     public void checkForUpdates(Frame parent) {
         if (isNewerVersionAvailable()) {
             log.debug("Newer version of JConvert found.");
-            String msg = "A newer version (" + webVersion + ") of JConvert is available.\r\nYour current version is "
-                    + currentVersion + ".\nDo you want to download the latest from the web?";
-            dlg = new JDialog(parent, "Newer Version Available", true);
+            dlg = new JDialog(parent, Messages.getResource("newVersionTitle"), true);
             dlg.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
             dlg.setSize(400, 200);
+            dlg.setLocationRelativeTo(parent);
             dlg.getContentPane().setLayout(null);
 
-            JTextArea label = new JTextArea(msg);
+            JTextArea label = new JTextArea(Messages.getResource("newVersionMsg", webVersion, currentVersion));
             label.setBounds(5, 5, 385, 60);
             label.setEditable(false);
-            label.setBackground(dlg.getBackground());
+            JLabel temp = new JLabel("temp");
+            label.setBackground(temp.getBackground());
             dlg.getContentPane().add(label);
 
-            JButton yesBtn = new JButton("Yes");
+            JButton yesBtn = new JButton(Messages.getResource("yesButton"));
             yesBtn.setBounds(5, 70, 100, 22);
             yesBtn.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
                     openLinkToWeb();
                     //TODO think about closing the application in some way...
                     //possibly change one of the buttons to "close application"
-                    //what about looking for the new jar? 
+                    //what about looking for the new jar?
                     //what about trying to delete the old jar if new exists?
                     //what about some buttons to do this?
                 }
             });
             dlg.getContentPane().add(yesBtn);
 
-            JButton noBtn = new JButton("No");
+            JButton noBtn = new JButton(Messages.getResource("noButton"));
             noBtn.setBounds(115, 70, 100, 22);
             noBtn.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -80,7 +82,7 @@ public class UpgradeVersionChecker {
             });
             dlg.getContentPane().add(noBtn);
 
-            JButton dontAskBtn = new JButton("No, and don't ask me anymore.");
+            JButton dontAskBtn = new JButton(Messages.getResource("dontAskButton"));
             dontAskBtn.setBounds(5, 100, 350, 22);
             dontAskBtn.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -130,17 +132,18 @@ public class UpgradeVersionChecker {
         Boolean val = compare(props.getProperty("MajorVersion"), JConvertProperties.getMajorVersion());
         if (val != null) {
             return val.booleanValue();
-        } 
+        }
         val = compare(props.getProperty("MinorVersion"), JConvertProperties.getMinorVersion());
         if (val != null) {
             return val.booleanValue();
-        } 
+        }
         val = compare(props.getProperty("Revision"), JConvertProperties.getRevision());
         if (val != null) {
             return val.booleanValue();
-        } 
+        }
         return false;
     }
+
     private Boolean compare(String first, String second) {
         int intFirst = new Integer(first).intValue();
         int intSecond = new Integer(second).intValue();
@@ -149,7 +152,7 @@ public class UpgradeVersionChecker {
         } else if (intFirst < intSecond) {
             return Boolean.FALSE;
         }
-            
+
         return null;
     }
 
