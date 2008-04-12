8f620678d619c695e1d46aff775089fca6f3d0ee
edsdev <edsdev@af8f7118-8b35-0410-841c-bf10d0a9d012>
2008-04-12 18:45:05 +0000

    New listener interface
    git-svn-id: https://jconvert.svn.sourceforge.net/svnroot/jconvert@151 af8f7118-8b35-0410-841c-bf10d0a9d012

---
+++ src/com/edsdev/jconvert/common/CustomDataUpdatedListener.java
@@ -0,0 +1,8 @@
+package com.edsdev.jconvert.common;
+
+/**
+ * @author Ed Sarrazin Created on Oct 29, 2007 5:22:43 PM
+ */
+public interface CustomDataUpdatedListener {
+    public void customDataUpdated();
+}
